5dbafa7dc10fff40cb8a7394958b929acac5c484
Arthur <arthurkarimullin2005@gmail.com>
2021-05-05 15:19:31 +0500

    add 'start'

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import pyautogui
 import time
 import os
 
-
-
-time.sleep(5)
-
 def main(): 
     z = '0'
     name = "0.png"
@@ -17,6 +13,25 @@ def main():
         time.sleep(0.5)
         pyautogui.hotkey('pagedown')
 
+def start():
+    print("5..")
+    time.sleep(1)
+    
+    print("4..")
+    time.sleep(1)
+    
+    print("3..")
+    time.sleep(1)
+    
+    print("2..")
+    time.sleep(1)
+    
+    print("1..")
+    time.sleep(1)
+
+    print("Screen!")
+
+start()
 main()
 
 
