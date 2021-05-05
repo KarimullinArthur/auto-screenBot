be744cf83e73554849856fd6fef8a7e8ccb1696b
Arthur <arthurkarimullin2005@gmail.com>
2021-05-05 14:34:21 +0500

    minor changes

--- main.py
+++ main.py
@@ -11,12 +11,11 @@ def main():
     name = "0.png"
     path = "./1/"
     os.mkdir(path)
-    for y in range(1,5):
 
-        for x in range(0,50):
-            pyautogui.screenshot(path+name)
-            time.sleep(0.5)
-            pyautogui.hotkey('pagedown')
+    for x in range(0,50):
+        pyautogui.screenshot(path+name)
+        time.sleep(0.5)
+        pyautogui.hotkey('pagedown')
 
 main()
 
